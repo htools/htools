79627a75ffbc909305428cf0bc43d9f3b0e45808
Jeroen Vuurens <j.b.p.vuurens@tudelft.nl>
2014-07-22 10:05:36 +0200

    Gamma and Chi2

--- src/main/java/io/github/repir/tools/Lib/MathTools.java
+++ src/main/java/io/github/repir/tools/Lib/MathTools.java
@@ -572,6 +572,10 @@ public class MathTools {
       return V;
    }
 
+   public static double chiSquared(int a, int b, int c, int d) {
+       return Math.pow(a * d - b * c, 2) * (a + b + c + d) / (double) ((a + b) * (c + d) * (a + c) * (b + d));
+   }
+   
    public static int mod(int x, int y) {
       int result = x % y;
       if (result < 0) {
@@ -635,6 +639,21 @@ public class MathTools {
       return result;
    }
 
+   /**
+    * Lanczos gamma approximation, from numerical recipes
+    * @param x
+    * @return 
+    */   
+   static double logGamma(double x) {
+      double tmp = (x - 0.5) * Math.log(x + 4.5) - (x + 4.5);
+      double ser = 1.0 + 76.18009173    / (x + 0)   - 86.50532033    / (x + 1)
+                       + 24.01409822    / (x + 2)   -  1.231739516   / (x + 3)
+                       +  0.00120858003 / (x + 4)   -  0.00000536382 / (x + 5);
+      return tmp + Math.log(ser * Math.sqrt(2 * Math.PI));
+   }
+   static double gamma(double x) { return Math.exp(logGamma(x)); }
+
+   
    public static int numberCoverBits(int v) {
       int l = Integer.lowestOneBit(v);
       int h = Integer.highestOneBit(v);
